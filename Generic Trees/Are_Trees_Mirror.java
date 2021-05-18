4b6b2e8676ac30d4182a37507d16956cc3bcefcd
imutkarsh09 <itsutkarsh2711@gmail.com>
2021-05-18 18:35:02 +0530

    Are trees mirror of each other added

---
+++ Generic Trees/Are_Trees_Mirror.java
@@ -0,0 +1,81 @@
+import java.util.*;
+public class Are_Trees_Mirror
+{
+    public static class Node
+    {
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+    }
+    public static boolean areMirror(Node n1, Node n2)
+    {
+        if(n1.children.size()!= n2.children.size())
+        {
+            return false;
+        }
+        for(int i=0;i<n1.children.size();i++)
+        {
+            int j=n1.children.size()-i-1;
+            Node c1=n1.children.get(i);
+            Node c2= n2.children.get(j);
+            if(areMirror(c1,c2)==false)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static Node construct(int[] Ar)
+    {
+        Stack<Node> st = new Stack<>();
+        Node root=null;
+        for(int i:Ar)
+        {
+            if(i==-1)
+            {
+                st.pop();
+            }
+            else
+            {
+                Node t = new Node();
+                t.data=i;
+                if(st.size()==0)
+                {
+                    root=t;
+                }
+                else
+                {
+                    st.peek().children.add(t);
+                }
+                st.push(t);
+            }
+        }
+        return root;
+    }
+    public static void main(String Args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] Ar = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            Ar[i]=sc.nextInt();
+        }
+        Node n1 = construct(Ar);
+        int n22=sc.nextInt();
+        int[] Ar1 = new int[n];
+        for(int i=0;i<n22;i++)
+        {
+            Ar1[i]=sc.nextInt();
+        }
+        Node n2 = construct(Ar1);
+        boolean ans = areMirror(n1, n2);
+        if(ans == true)
+        {
+            System.out.println("Trees are mirror");
+        }
+        else
+        {
+            System.out.println("Trees are not mirror");
+        }
+    }
+}

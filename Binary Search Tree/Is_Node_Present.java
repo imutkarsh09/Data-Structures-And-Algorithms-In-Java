ac8f788755efd2916170afc737b8e0e889b30963
imutkarsh09 <itsutkarsh2711@gmail.com>
2021-06-22 17:11:13 +0530

    Is node present in Binary Search Tree added

---
+++ Binary Search Tree/Is_Node_Present.java
@@ -0,0 +1,111 @@
+import java.util.*;
+
+import javax.lang.model.util.ElementScanner6;
+public class Is_Node_Present
+{
+    public static class Node
+    {
+        int data;
+        Node left;
+        Node right;
+        Node(int data, Node left, Node right)
+        {
+            this.data= data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public static class Pair
+    {
+        Node node;
+        int state;
+        Pair(Node node, int state)
+        {
+            this.node = node;
+            this.state = state;
+        }
+    }
+    public static Node construct(int[] Ar)
+    {
+        Node root = new Node(Ar[0], null,null);
+        Pair rtp = new Pair(root,1);
+        Stack<Pair> st = new Stack<>();
+        st.push(rtp);
+        int ind=0;
+        while(st.size()>0)
+        {
+            Pair top = st.peek();
+            if(top.state==1)
+            {
+                ind++;
+                if(Ar[ind]!=-1)
+                {
+                    Node ln = new Node(Ar[ind],null,null);
+                    top.node.left = ln;
+                    Pair lp = new Pair(ln,1);
+                    st.push(lp);
+                }
+                else
+                {
+                    top.node.left=null;
+                }
+                top.state++;
+            }
+            else if(top.state==2)
+            {
+                ind++;
+                if(Ar[ind]!=-1)
+                {
+                    Node rn = new Node(Ar[ind],null,null);
+                    top.node.right = rn;
+                    Pair rp = new Pair(rn,1);
+                    st.push(rp);
+                }
+                else
+                {
+                    top.node.right=null;
+                }
+                top.state++;
+            }
+            else
+            {
+                st.pop();
+            }
+        }
+        return root;
+    }
+    public static boolean isPresent(Node node, int data)
+    {
+        if(node==null)
+        {
+            return false;
+        }
+        if(data>node.data)
+        {
+            return isPresent(node.right, data);
+        }
+        else if(data<node.data)
+        {
+            return isPresent(node.left, data);
+        }
+        else
+        {
+            return true;
+        }
+    }
+    public static void main(String[] Args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] Ar = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            Ar[i]=sc.nextInt();
+        }
+        Node root = construct(Ar);
+        System.out.println("Enter Node to find");
+        int data =sc.nextInt();
+        boolean isPresent = isPresent(root, data);
+        System.out.println(isPresent);
+    }
+}

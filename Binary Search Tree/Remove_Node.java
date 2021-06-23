8ef014f3961af9eb9d62bf203f7944d63fb368ef
imutkarsh09 <itsutkarsh2711@gmail.com>
2021-06-23 14:17:29 +0530

    Remove node from BST added

---
+++ Binary Search Tree/Remove_Node.java
@@ -0,0 +1,170 @@
+import java.util.*;
+public class Remove_Node
+{
+    public static class Node
+    {
+        int data;
+        Node left;
+        Node right;
+        Node(int data, Node left, Node right)
+        {
+            this.data= data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public static class Pair
+    {
+        Node node;
+        int state;
+        Pair(Node node, int state)
+        {
+            this.node = node;
+            this.state = state;
+        }
+    }
+    public static Node construct(int[] Ar)
+    {
+        Node root = new Node(Ar[0], null,null);
+        Pair rtp = new Pair(root,1);
+        Stack<Pair> st = new Stack<>();
+        st.push(rtp);
+        int ind=0;
+        while(st.size()>0)
+        {
+            Pair top = st.peek();
+            if(top.state==1)
+            {
+                ind++;
+                if(Ar[ind]!=-1)
+                {
+                    Node ln = new Node(Ar[ind],null,null);
+                    top.node.left = ln;
+                    Pair lp = new Pair(ln,1);
+                    st.push(lp);
+                }
+                else
+                {
+                    top.node.left=null;
+                }
+                top.state++;
+            }
+            else if(top.state==2)
+            {
+                ind++;
+                if(Ar[ind]!=-1)
+                {
+                    Node rn = new Node(Ar[ind],null,null);
+                    top.node.right = rn;
+                    Pair rp = new Pair(rn,1);
+                    st.push(rp);
+                }
+                else
+                {
+                    top.node.right=null;
+                }
+                top.state++;
+            }
+            else
+            {
+                st.pop();
+            }
+        }
+        return root;
+    }
+    public static int max(Node node)
+    {
+        if(node.right!=null)
+        {
+            return max(node.right);
+        }
+        else
+        {
+            return node.data;
+        }
+    }
+    public static Node remove(Node node, int data)
+    {
+        if(node == null)
+        {
+            return null;
+        }
+        if(data>node.data)
+        {
+            node.right =  remove(node.right, data);
+        }
+        else if(data<node.data)
+        {
+            node.left =  remove(node.left, data);
+        }
+        else
+        {
+            if(node.left!=null && node.right!=null)
+            {
+                int lmax = max(node.left);
+                node.data = lmax;
+                node.left = remove(node.left, lmax);
+                return node;
+
+            }
+            else if(node.left!=null)
+            {
+                return node.left;
+            }
+            else if(node.right!=null)
+            {
+                return node.right;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        return node;
+    }
+    public static void display(Node node)
+    {
+        if(node==null)
+        {
+            return;
+        }
+        String st = "";
+        if(node.left!=null)
+        {
+            st = st+node.left.data;
+        }
+        else
+        {
+            st = st+".";
+        }
+        st = st+" <- "+node.data+" -> ";
+        if(node.right!=null)
+        {
+            st = st+node.right.data;
+        }
+        else
+        {
+            st = st+".";
+        }
+        System.out.println(st);
+        display(node.left);
+        display(node.right);
+
+    }
+    public static void main(String[] Args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] Ar = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            Ar[i]=sc.nextInt();
+        }
+        Node root = construct(Ar);
+        System.out.println("Enter Node to Remove");
+        int data =sc.nextInt();
+        Node rnode = remove(root, data);
+        display(rnode);
+
+    }
+}

9ac9b9f702f5fe920d6c16337f0f92397f6e0bce
Utkarsh Saxena <63578366+imutkarsh09@users.noreply.github.com>
2021-07-21 00:47:35 +0530

    Update Count_Stairs_With_Minimum_Moves.java

--- Dynamic Programming/Count_Stairs_With_Minimum_Moves.java
+++ Dynamic Programming/Count_Stairs_With_Minimum_Moves.java
@@ -1,3 +1,11 @@
+/*
+We are given a number n, representing the number of stairs in a staircase.
+In starting we are on the 0th step and are required to climb to the top.
+We are given n numbers, where ith element's value represents - till how far from the step you 
+    could jump to in a single move(ranging from 1 to Ar[i]).
+We have to print the number of minimum moves in which you can reach the top of 
+     staircase.
+ */
 import java.util.*;
 public class Count_Stairs_With_Minimum_Moves
 {

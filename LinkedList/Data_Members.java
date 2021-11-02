96a112adee4b089808c4195c60c0fec3da909e48
imutkarsh09 <itsutkarsh2711@gmail.com>
2021-11-02 16:18:34 +0530

    Data Members in a LinkedList added

---
+++ LinkedList/Data_Members.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Data_Members
+{
+    public class Node
+    {
+        int data;
+        Node next;
+    }
+    public class LinkedList
+    {
+        Node head;
+        Node tail;
+        int size;
+    }
+    public static void main(String[] Args)
+    {
+
+    }
+}
